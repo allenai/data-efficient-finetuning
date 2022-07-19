89c1b7a6c4f764af97e9f7ab2158a7e372319fec
pdasigi <pradeepd@allenai.org>
2022-07-19 11:13:31 -0700

    parallel encoder and other features in retriever

--- scripts/retrieve_training_data.py
+++ scripts/retrieve_training_data.py
@@ -2,6 +2,7 @@ import argparse
 import faiss
 import torch
 import json
+import gzip
 import tqdm
 from collections import defaultdict
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
@@ -12,70 +13,88 @@ from attribution.qasper_reader import QasperEvidencePromptReader
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--dev_data", type=str, required=True)
+parser.add_argument("--dev_data", type=str)
 parser.add_argument("--negative_sample_ratio", type=float, default=1.0)
-parser.add_argument("--index", type=str, required=True)
-parser.add_argument("--model", type=str, required=True)
+parser.add_argument("--index", type=str)
+parser.add_argument("--model", type=str)
 parser.add_argument("--search_output", type=str, required=True)
 parser.add_argument("--batch_size", type=int, default=4)
 parser.add_argument("--num_neighbors_search", type=int, default=100)
 parser.add_argument("--p3_data", type=str, help="If provided, will write training data to `training_data`")
 parser.add_argument("--training_data", type=str)
 parser.add_argument("--num_neighbors_write", type=int, default=20)
+parser.add_argument("--write_positive_neighbors_only", action="store_true", help="If set, will write neighbors of positive dev instances alone")
 parser.add_argument("--p3_dataset_indices", type=str, help="If provided, will compute P3 dataset stats")
 parser.add_argument("--stats_log", type=str, help="File to write the dataset stats")
+parser.add_argument("--cuda_devices", type=int, nargs="+")
 args = parser.parse_args()
 
 
-reader = QasperEvidencePromptReader(model_name=args.model, negative_sample_ratio=args.negative_sample_ratio)
-tokenizer = AutoTokenizer.from_pretrained(args.model)
-model = AutoModelForSeq2SeqLM.from_pretrained(args.model)
-if torch.cuda.is_available():
-    model.cuda()
-model.eval()
-index = faiss.read_index(args.index)
-
-def query_index(queries):
-    input_data = tokenizer.batch_encode_plus(queries,
-                                             return_tensors="pt",
-                                             padding=True)
-    input_ids = input_data['input_ids']
-    # (batch_size, num_tokens)
-    mask = input_data['attention_mask']
-    if torch.cuda.is_available():
-        input_ids = input_ids.cuda()
-        mask = mask.cuda()
-    encoder_outputs = model.encoder(input_ids=input_ids,
-                                       attention_mask=mask,
-                                       return_dict=True)
-    # (batch_size, num_tokens, hidden_size)
-    hidden_states = encoder_outputs["last_hidden_state"]
-    # (batch_size, hidden_size)
-    pooled_hidden_states = (hidden_states * mask.unsqueeze(-1)).sum(1) / mask.sum(1).unsqueeze(-1)
-    pooled_hidden_states_np = pooled_hidden_states.detach().cpu().numpy()
-    return index.search(pooled_hidden_states_np, k=args.num_neighbors_search)
-
-
 indices_frequencies = defaultdict(int)
-outputfile = open(args.search_output, "w")
-batch = []
-with torch.inference_mode():
-    for instance in tqdm.tqdm(reader.read(args.dev_data)):
-        metadata = instance.fields['metadata'].metadata
-        batch.append({"question_id": metadata["question_id"], "query": metadata["query"], "paragraph_index": metadata["paragraph_index"]})
-        if len(batch) == args.batch_size:
-            batch_distances, batch_indices = query_index([i["query"] for i in batch])
-            for instance, distances, indices in zip(batch, batch_distances, batch_indices):
-                ids = [int(id_) for id_ in indices]
-                for id_ in ids[:args.num_neighbors_write]:
-                    indices_frequencies[id_] += 1
-                distances = [float(distance) for distance in distances]
-                datum = {"question_id": instance["question_id"], "paragraph_index": instance["paragraph_index"], "ids": ids, "distances": distances}
-                print(json.dumps(datum), file=outputfile)
-            outputfile.flush()
-            batch = []
+if not os.path.exists(args.search_output):
+    assert args.dev_data is not None
+    assert args.index is not None
+    assert args.model is not None
+    reader = QasperEvidencePromptReader(model_name=args.model, negative_sample_ratio=args.negative_sample_ratio)
+    tokenizer = AutoTokenizer.from_pretrained(args.model)
+    model = AutoModelForSeq2SeqLM.from_pretrained(args.model)
+    cuda_devices = args.cuda_devices or list(range(torch.cuda.device_count()))
+    print(f"Using CUDA devices {cuda_devices}")
+    if torch.cuda.is_available():
+        model.cuda(device=cuda_devices[0])
+    model.eval()
+    encoder = torch.nn.DataParallel(model.encoder, device_ids=cuda_devices)
+    index = faiss.read_index(args.index)
 
-print("\nDone searching.")
+    def query_index(queries):
+        input_data = tokenizer.batch_encode_plus(queries,
+                                                 return_tensors="pt",
+                                                 padding=True)
+        input_ids = input_data['input_ids']
+        # (batch_size, num_tokens)
+        mask = input_data['attention_mask']
+        if torch.cuda.is_available():
+            input_ids = input_ids.cuda(device=cuda_devices[0])
+            mask = mask.cuda(device=cuda_devices[0])
+        encoder_outputs = encoder(input_ids=input_ids,
+                                  attention_mask=mask,
+                                  return_dict=True)
+        # (batch_size, num_tokens, hidden_size)
+        hidden_states = encoder_outputs["last_hidden_state"]
+        # (batch_size, hidden_size)
+        pooled_hidden_states = (hidden_states * mask.unsqueeze(-1)).sum(1) / mask.sum(1).unsqueeze(-1)
+        pooled_hidden_states_np = pooled_hidden_states.detach().cpu().numpy()
+        return index.search(pooled_hidden_states_np, k=args.num_neighbors_search)
+
+
+    outputfile = open(args.search_output, "w")
+    batch = []
+    with torch.inference_mode():
+        for instance in tqdm.tqdm(reader.read(args.dev_data)):
+            metadata = instance.fields['metadata'].metadata
+            batch.append({"question_id": metadata["question_id"], "query": metadata["query"], "paragraph_index": metadata["paragraph_index"], "target": metadata["target"]})
+            if len(batch) == args.batch_size:
+                batch_distances, batch_indices = query_index([i["query"] for i in batch])
+                for instance_, distances, indices in zip(batch, batch_distances, batch_indices):
+                    ids = [int(id_) for id_ in indices]
+                    if not args.write_positive_neighbors_only or "Yes" in instance_["target"]:
+                        for id_ in ids[:args.num_neighbors_write]:
+                            indices_frequencies[id_] += 1
+                    distances = [float(distance) for distance in distances]
+                    datum = {"question_id": instance_["question_id"], "paragraph_index": instance_["paragraph_index"], "target": instance_["target"], "ids": ids, "distances": distances}
+                    print(json.dumps(datum), file=outputfile)
+                outputfile.flush()
+                batch = []
+
+    print("\nDone searching.")
+else:
+    print("Search output exists. Reading it instead of querying the index.")
+    retrieved_data = [json.loads(line) for line in open(args.search_output)]
+    for datum in retrieved_data:
+        if not args.write_positive_neighbors_only or "Yes" in datum["target"]:
+            for id_ in datum["ids"][:args.num_neighbors_write]:
+                indices_frequencies[id_] += 1
+
 max_freq_indices = sorted(indices_frequencies.items(), key=lambda x: x[1], reverse=True)[:10]
 print(f"\nMost frequent indices: {max_freq_indices}")
 
@@ -83,7 +102,7 @@ max_index = max(indices_frequencies.keys())
 
 if args.p3_data:
     with open(args.training_data, "w") as outfile:
-        for i, line in enumerate(open(args.p3_data)):
+        for i, line in enumerate(gzip.open(args.p3_data, "rt")):
             if i > max_index:
                 break
             if i in indices_frequencies:
@@ -96,7 +115,7 @@ print("\nDone writing training data")
 
 if args.p3_dataset_indices:
     dataset_stats = defaultdict(lambda: {"seen": 0, "attributed": 0})
-    for i, line in enumerate(open(args.p3_dataset_indices)):
+    for i, line in enumerate(gzip.open(args.p3_dataset_indices, "rt")):
         if i > max_index:
             break
         dataset_name, _, _ = line.strip().split("\t")
