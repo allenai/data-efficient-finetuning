a40fb9636517c31a2b4d41c341291a141c526c19
Hamish Ivison <hamishivi@gmail.com>
2022-09-17 20:12:06 +0000

    add new readers, split out drop

--- attribution/huggingface_readers.py
+++ attribution/huggingface_readers.py
@@ -2,7 +2,7 @@ import json
 import logging
 import random
 from collections import defaultdict
-from typing import List, Iterable, Optional, Tuple
+from typing import List, Iterable, Optional, Tuple, Dict
 
 from overrides import overrides
 import datasets
@@ -30,6 +30,7 @@ class HuggingfaceReaderRankClassification(DatasetReader):
         max_query_length: int = 512,
         split_name: str = "train",
         val_size: int = 1000,
+        split_mapping: Dict[str, str] = {"train": "train", "validation": "validation"},
         **kwargs,
     ) -> None:
         super().__init__(
@@ -38,11 +39,15 @@ class HuggingfaceReaderRankClassification(DatasetReader):
             **kwargs,
         )
         dataset_name, subset_name = self.get_dataset_name()
+        data_dir = self.get_dataset_dir()  # for story cloze
         self._dataset_name = dataset_name
         self._subset_name = subset_name
         self.split_name = split_name
         original_val_set = datasets.load_dataset(
-            dataset_name, subset_name, split="validation"
+            dataset_name,
+            subset_name,
+            split=split_mapping["validation"],
+            data_dir=data_dir
         )
         small_val_size = val_size  # I consider under 1000 examples as small
         val_split_size = val_size  # when splitting out val, get 1000 examples
@@ -50,14 +55,14 @@ class HuggingfaceReaderRankClassification(DatasetReader):
         if split_name == "train":
             if len(original_val_set) >= small_val_size:
                 self._dataset = datasets.load_dataset(
-                    dataset_name, subset_name, split="train"
+                    dataset_name, subset_name, split=split_mapping["train"], data_dir=data_dir
                 )
             else:
                 # for small val sets, split out val from train and use old val as test
                 # this is because some casehold splits are specially designed, so I want
                 # to keep these as-is (rather than just split the val set in half)
                 self._dataset = datasets.load_dataset(
-                    dataset_name, subset_name, split="train"
+                    dataset_name, subset_name, split=split_mapping["train"], data_dir=data_dir
                 ).train_test_split(test_size=val_split_size, seed=seed)["train"]
         if split_name == "validation":
             # for large val sets, just split out from val
@@ -68,7 +73,7 @@ class HuggingfaceReaderRankClassification(DatasetReader):
             else:
                 # for small val sets, split out val from train and use old val as test
                 self._dataset = datasets.load_dataset(
-                    dataset_name, subset_name, split="train"
+                    dataset_name, subset_name, split=split_mapping["train"], data_dir=data_dir
                 ).train_test_split(test_size=val_split_size, seed=seed)["test"]
         elif split_name == "test":
             # for large val sets, test is the small split from val
@@ -79,7 +84,7 @@ class HuggingfaceReaderRankClassification(DatasetReader):
             else:
                 # for small val sets, split our new val from train (val becomes test)
                 self._dataset = datasets.load_dataset(
-                    dataset_name, subset_name, split="validation"
+                    dataset_name, subset_name, split=split_mapping["validation"], data_dir=data_dir
                 )
         if split_name == "train":
             self._dataset = self._dataset.shuffle(42)
@@ -95,6 +100,10 @@ class HuggingfaceReaderRankClassification(DatasetReader):
 
     def get_dataset_name(self) -> Tuple[str, Optional[str]]:
         raise NotImplementedError("Specify ds name for hf")
+    
+    # we usually dont need this, but story cloze requires it.
+    def get_dataset_dir(self) -> Optional[str]:
+        return None
 
     @overrides
     def _read(self, file_path) -> Iterable[Instance]:
@@ -115,16 +124,19 @@ class HuggingfaceReaderRankClassification(DatasetReader):
 
         input_field = TextField(tokenized_input)
         fields["prompt_and_input"] = input_field
+        fields["prompt_and_input_pretokenized"] = input_text
 
         answer_option_fields = [
             TextField(self._tokenizer.tokenize(option)) for option in answer_options
         ]
         options_list_field = ListField(answer_option_fields)
         fields["answer_options"] = options_list_field
+        fields["answer_options_pretokenized"] = answer_options
 
         fields["correct_answer_index"] = IndexField(
             correct_answer_idx, options_list_field
         )
+        fields["correct_answer_index_value"] = correct_answer_idx
 
         return Instance(fields)
 
@@ -279,136 +291,179 @@ class RTEReader(HuggingfaceReaderRankClassification):
         return [[input, answers, correct_answer]]
 
 
-# full reader for drop to pass through query id
-@DatasetReader.register("drop_reader")
-class DROPReader(DatasetReader):
+# CB
+@DatasetReader.register("cb_reader")
+class CBReader(HuggingfaceReaderRankClassification):
+    def get_dataset_name(self) -> Tuple[str, Optional[str]]:
+        return "super_glue", "cb"
+
+    def hf_to_instance(self, instance) -> Tuple[str, str]:
+        # using gpt-3 style prompt
+        input = f"{instance['premise']}\nQuestion: {instance['hypothesis']} True, False, or Neither?"
+        # 0 = entail, 1 = contradict, 2 = neutral
+        answers = ["true", "false", "neither"]
+        correct_answer = int(instance["label"])
+        return [[input, answers, correct_answer]]
+
+
+# HellaSwag
+@DatasetReader.register("hellaswag_reader")
+class HellaSwagReader(HuggingfaceReaderRankClassification):
+    def get_dataset_name(self) -> Tuple[str, Optional[str]]:
+        return "hellaswag", "None"
+
+    def hf_to_instance(self, instance) -> Tuple[str, str]:
+        input = f"Complete the description with an appropriate ending:\nFirst, {instance['ctx_a']} Then, {instance['ctx_b']} ...\n(a) {instance['endings'][0]}\n(b) {instance['endings'][1]}\n(c) {instance['endings'][2]}\n(d) {instance['endings'][3]}"
+        answers = instance['endings']
+        correct_answer = int(instance['label'])
+        return [[input, answers, correct_answer]]
+
+
+# StoryCloze
+## NB: requires downloading separately as it requires agreeing to a thing
+# 
+@DatasetReader.register("story_cloze_reader")
+class StoryClozeReader(HuggingfaceReaderRankClassification):
+    def get_dataset_name(self) -> Tuple[str, Optional[str]]:
+        return "story_cloze", "2016"
+
+    # we usually dont need this, but story cloze requires it.
+    def get_dataset_dir(self) -> Optional[str]:
+        return '/net/nfs.cirrascale/allennlp/hamishi/downloaded_story_cloze/'
+
+    def hf_to_instance(self, instance) -> Tuple[str, str]:
+        input = f"{instance['input_sentence_1']} {instance['input_sentence_2']} {instance['input_sentence_3']} {instance['input_sentence_4']} What is a possible continuation for the story given the following options ?\n- {instance['sentence_quiz1']}\n- {instance['sentence_quiz2']}"
+        answers = [instance['sentence_quiz1'], instance['sentence_quiz2']]
+        correct_answer = instance['answer_right_ending']
+        return [[input, answers, correct_answer]]
+
+# WinoGrande
+@DatasetReader.register("winogrande_reader")
+class WinoGrandeReader(HuggingfaceReaderRankClassification):
+    def get_dataset_name(self) -> Tuple[str, Optional[str]]:
+        return "winogrande", "winogrande_xl"
+
+    def hf_to_instance(self, instance) -> Tuple[str, str]:
+        # using underscore refer to
+        input = f"{instance['sentence']}\nWhat does the _ in the above sentence refer to? {instance['option1']} or {instance['option2']}?"
+        # 0 = false, 1 = true
+        answers = ['1', '2']
+        assert instance['answer'] in answers
+        correct_answer = answers.index(instance['answer'])
+        return [[input, answers, correct_answer]]
+
+# WSC
+@DatasetReader.register("wsc_reader")
+class WSCReader(HuggingfaceReaderRankClassification):
+    def get_dataset_name(self) -> Tuple[str, Optional[str]]:
+        return "super_glue", "wsc"
+
+    def hf_to_instance(self, instance) -> Tuple[str, str]:
+        # using gpt3 style
+        input = f"Passage: {instance['text']} \nQuestion: In the passage above, does the pronoun \"{instance['span2_text']}\" refer to {instance['span1_text']}?\nAnswer:"
+        # 0 = false, 1 = true
+        answers = ['False', 'True']
+        correct_answer = int(instance["label"])
+        return [[input, answers, correct_answer]]
+
+# COPA
+@DatasetReader.register("copa_reader")
+class COPAReader(HuggingfaceReaderRankClassification):
+    def get_dataset_name(self) -> Tuple[str, Optional[str]]:
+        return "super_glue", "copa"
+
+    def hf_to_instance(self, instance) -> Tuple[str, str]:
+        # using using 'plausible alternatives' prompt
+        input = f"{instance['premise']}  As a consequence... \nHelp me pick the more plausible option:\n- {instance['choice1']}\n- {instance['choice2']}"
+        # 0 = choice1, 1 = choice2
+        answers = [instance['choice1'], instance['choice2']]
+        correct_answer = int(instance["label"])
+        return [[input, answers, correct_answer]]
+
+# WiC
+@DatasetReader.register("wic_reader")
+class WiCReader(HuggingfaceReaderRankClassification):
+    def get_dataset_name(self) -> Tuple[str, Optional[str]]:
+        return "super_glue", "wic"
+
+    def hf_to_instance(self, instance) -> Tuple[str, str]:
+        # using gpt-3 style prompt
+        input = f"{instance['sentence1']}\n{instance['sentence2']}\nQuestion: Is the word '{instance['word']}' used in the same sense in the two sentences above? Yes, No?"
+        # 0 = false, 1 = true
+        answers = ["no", "yes"]
+        correct_answer = int(instance["label"])
+        return [[input, answers, correct_answer]]
+
+## anli is handled specially as its splits have weird names.
+
+# ANLI R1
+@DatasetReader.register("anli_r1_reader")
+class ANLIR1Reader(HuggingfaceReaderRankClassification):
     def __init__(
         self,
-        model_name: str = "google/t5-small-lm-adapt",
-        max_query_length: int = 512,
-        split_name: str = "train",
-        val_size: int = 1000,
+        split_mapping={"train": "train_r1", "validation": "dev_r1"},
+        **kwargs,
+    ) -> None:
+        super().__init__(split_mapping=split_mapping, **kwargs)
+
+    def get_dataset_name(self) -> Tuple[str, Optional[str]]:
+        return "anli", None
+
+    def hf_to_instance(self, instance) -> Tuple[str, str]:
+        # using gpt-3 style prompt
+        input = f"{instance['premise']}\nQuestion: {instance['hypothesis']} True, False, or Neither?"
+        # 0 = entail, 1 = neutral, 2 = contradiction
+        answers = ["true", "neither", 'false']
+        correct_answer = instance['label']
+        return [[input, answers, correct_answer]]
+
+
+# the other anlis are identical, just diff splits :)
+# ANLI R2
+@DatasetReader.register("anli_r2_reader")
+class ANLIR2Reader(ANLIR1Reader):
+    def __init__(
+        self,
         **kwargs,
     ) -> None:
         super().__init__(
-            manual_distributed_sharding=True,
-            manual_multiprocess_sharding=True,
+            split_mapping={"train": "train_r2", "validation": "dev_r2"},
             **kwargs,
         )
-        dataset_name, subset_name = self.get_dataset_name()
-        self._dataset_name = dataset_name
-        self._subset_name = subset_name
-        original_val_set = datasets.load_dataset(
-            dataset_name, subset_name, split="validation"
-        )
-        small_val_size = val_size  # I consider under 2000 examples as small
-        val_split_size = val_size  # when splitting out val, get 1000 examples
-        seed = 42
-        if split_name == "train":
-            if len(original_val_set) >= small_val_size:
-                self._dataset = datasets.load_dataset(
-                    dataset_name, subset_name, split="train"
-                )
-            else:
-                # for small val sets, split out val from train and use old val as test
-                # this is because some casehold splits are specially designed, so I want
-                # to keep these as-is (rather than just split the val set in half)
-                self._dataset = datasets.load_dataset(
-                    dataset_name, subset_name, split="train"
-                ).train_test_split(test_size=val_split_size, seed=seed)["train"]
-        if split_name == "validation":
-            # for large val sets, just split out from val
-            if len(original_val_set) >= small_val_size:
-                self._dataset = original_val_set.train_test_split(
-                    train_size=val_split_size, seed=seed
-                )["train"]
-            else:
-                # for small val sets, split out val from train and use old val as test
-                self._dataset = datasets.load_dataset(
-                    dataset_name, subset_name, split="train"
-                ).train_test_split(test_size=val_split_size, seed=seed)["test"]
-        elif split_name == "test":
-            # for large val sets, test is the small split from val
-            if len(original_val_set) >= small_val_size:
-                self._dataset = original_val_set.train_test_split(
-                    train_size=val_split_size, seed=seed
-                )["test"]
-            else:
-                # for small val sets, split our new val from train (val becomes test)
-                self._dataset = datasets.load_dataset(
-                    dataset_name, subset_name, split="validation"
-                )
-        self._transformer_model_name = model_name
-        self._tokenizer = PretrainedTransformerTokenizer(model_name)
 
-        self._token_indexers = {"tokens": PretrainedTransformerIndexer(model_name)}
-        self._max_query_length = max_query_length
-        self._stats = defaultdict(int)
-
-    def get_dataset_name(self) -> Tuple[str, Optional[str]]:
-        return "drop", None
-
-    def hf_to_instance(self, instance) -> Tuple[str, str]:
-        # using GPT-3 DROP prompt.
-        input = (
-            f"Passage: {instance['passage']}\nQuestion: {instance['question']}\nAnswer:"
-        )
-        answer = instance["answers_spans"]["spans"][0]
-        return [[input, instance["query_id"], answer]]
-
-    @overrides
-    def _read(self, file_path) -> Iterable[Instance]:
-        for sample in self._dataset:
-            converted_samples = self.hf_to_instance(sample)
-            for inputs, qid, targets in converted_samples:
-                yield self.text_to_instance(inputs, qid, targets)
-
-    def text_to_instance(
+# ANLI R3
+@DatasetReader.register("anli_r3_reader")
+class ANLIR3Reader(ANLIR1Reader):
+    def __init__(
         self,
-        input_text: str,
-        query_id: str,
-        target: str,
-    ) -> Instance:
-        fields = {}
-
-        tokenized_input = self._tokenizer.tokenize(input_text)
-        if len(tokenized_input) > self._max_query_length:
-            self._stats["Truncated inputs"] += 1
-            tokenized_input = tokenized_input[: self._max_query_length]
-
-        input_field = TextField(tokenized_input)
-        fields["prompt_and_input"] = input_field
-        # fields["pretokenized_input"] = input_text
-
-        tokenized_target = self._tokenizer.tokenize(target)
-        if len(tokenized_target) > self._max_query_length:
-            self._stats["Truncated targets"] += 1
-            tokenized_target = tokenized_target[: self._max_query_length]
-
-        target_field = TextField(tokenized_target)
-        fields["target"] = target_field
-
-        query_id_field = MetadataField(query_id)
-        fields["query_id"] = query_id_field
-
-        return Instance(fields)
-
-    @overrides
-    def apply_token_indexers(self, instance: Instance) -> None:
-        instance.fields["prompt_and_input"].token_indexers = self._token_indexers
-        instance.fields["target"].token_indexers = self._token_indexers
-
-
-# drop reader that aligns with the other formats for multitask
-# NOTE: don't use this for eval!
-@DatasetReader.register("multi_task_drop_reader")
-class DropMReader(HuggingfaceReaderRankClassification):
-    def get_dataset_name(self) -> Tuple[str, Optional[str]]:
-        return "drop", None
-
-    def hf_to_instance(self, instance) -> Tuple[str, str]:
-        input = (
-            f"Passage: {instance['passage']}\nQuestion: {instance['question']}\nAnswer:"
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            split_mapping={"train": "train_r3", "validation": "dev_r3"},
+            **kwargs,
         )
-        answer = [instance["answers_spans"]["spans"][0]]
-        return [[input, answer, 0]]
+
+
+# to generate files containing training data
+# easy to repurpose to generate whatever you want.
+if __name__ == '__main__':
+    import json
+    data_classes = [StoryClozeReader, RTEReader, CBReader, HellaSwagReader, COPAReader, WinoGrandeReader, WSCReader, WiCReader, ANLIR1Reader, ANLIR2Reader, ANLIR3Reader]
+    data_names = ['story_cloze', 'rte', 'cb', 'hellaswag', 'copa', 'winogrande', 'wsc', 'wic', 'anli_r1', 'anli_r2', 'anli_r3']
+    for cls, name in zip(data_classes, data_names):
+        reader = cls(
+            model_name="google/t5-large-lm-adapt",
+            max_query_length=512,
+            split_name="test",
+            val_size=1e100,  # avoid splitting train set rn.
+        )
+        lines = []
+        for sample in reader.read('dummy'):
+            lines.append(json.dumps({
+                "prompt_and_input": sample['prompt_and_input_pretokenized'],
+                "answer_options": sample['answer_options_pretokenized'],
+                'correct_answer_index': sample['correct_answer_index_value']
+            }))
+        with open(f'data/{name}_val_data.jsonl', 'w') as f:
+            f.write('\n'.join(lines))
+    
